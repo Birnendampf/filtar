b7c1eb4f05a0632148590cc9914acad3e8f71291
Pearsteam <93873756+Birnendampf@users.noreply.github.com>
2026-03-23 16:17:02 +0100

    use Cow Path (it avoids copies if strings are passed as arguments)

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,20 @@ use pyo3::prelude::*;
 #[pymodule]
 mod filtar {
     use pyo3::prelude::*;
+    use std::borrow::Cow;
     use std::fs::File;
-    use std::path::PathBuf;
+    use std::path::{Path, PathBuf};
 
-    /// Formats the sum of two numbers as string.
+    /// extract a tar file to dest while skipping files and directories from exclude
     #[pyfunction]
     #[pyo3(signature = (src, dest, exclude = None))]
     fn extract(
         py: Python,
-        src: PathBuf,
-        dest: PathBuf,
+        src: Cow<Path>,
+        dest: Cow<Path>,
         exclude: Option<Bound<PyAny>>,
     ) -> PyResult<()> {
-        let exclude = match exclude {
-            Some(obj) => obj
-                .try_iter()?
-                .map(|elem| elem.and_then(|elem| elem.extract()))
-                .collect::<PyResult<Box<[PathBuf]>>>()?,
-            None => Box::default(),
-        };
+        let exclude = process_exclude(exclude)?;
         py.detach(|| {
             let mut a = tar::Archive::new(zstd::Decoder::new(File::open(src)?)?);
             for file in a.entries()? {
@@ -38,5 +33,15 @@ mod filtar {
             }
             Ok(())
         })
+    }
+
+    fn process_exclude(exclude: Option<Bound<PyAny>>) -> PyResult<Vec<PathBuf>> {
+        match exclude {
+            Some(obj) => obj
+                .try_iter()?
+                .map(|elem| elem.and_then(|elem| elem.extract()))
+                .collect(),
+            None => Ok(Vec::new()),
+        }
     }
 }
