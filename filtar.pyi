8f67490d9003864232c5e5597024608cdeebf333
Pearsteam <93873756+Birnendampf@users.noreply.github.com>
2026-03-23 18:30:00 +0100

    add archive creation

--- filtar.pyi
+++ filtar.pyi
@@ -1,5 +1,8 @@
 from _typeshed import StrPath
-from collections.abc import Iterable
+from collections.abc import Iterable, Set
 
 
 def extract(src: StrPath, dest: StrPath, exclude: Iterable[StrPath] | None = None) -> None: ...
+
+
+def create(src: StrPath, dest: StrPath, exclude: Set[str] = ...) -> None: ...
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,10 @@ use pyo3::prelude::*;
 mod filtar {
     use pyo3::prelude::*;
     use std::borrow::Cow;
-    use std::fs::File;
+    use std::collections::HashSet;
+    use std::ffi::OsString;
+    use std::fs;
+    use std::io;
     use std::path::{Path, PathBuf};
 
     /// extract a tar file to dest while skipping files and directories from exclude
@@ -20,7 +23,8 @@ mod filtar {
     ) -> PyResult<()> {
         let exclude = process_exclude(exclude)?;
         py.detach(|| {
-            let mut a = tar::Archive::new(zstd::Decoder::new(File::open(src)?)?);
+            fs::create_dir_all(&dest)?;
+            let mut a = tar::Archive::new(zstd::Decoder::new(fs::File::open(src)?)?);
             for file in a.entries()? {
                 let mut file = file?;
                 let path = file.path()?;
@@ -43,5 +47,46 @@ mod filtar {
                 .collect(),
             None => Ok(Vec::new()),
         }
+    }
+
+    /// create a tar archive to dest while skipping files and directories from exclude
+    #[pyfunction]
+    #[pyo3(signature = (src, dest, exclude = HashSet::new()))]
+    fn create(
+        py: Python,
+        src: Cow<Path>,
+        dest: Cow<Path>,
+        exclude: HashSet<OsString>,
+    ) -> io::Result<()> {
+        py.detach(|| {
+            let mut a = tar::Builder::new(zstd::Encoder::new(fs::File::create(dest)?, 0)?);
+            a.follow_symlinks(false);
+            // XXX: it is possible that simple slice / vector may be faster here (no hashing)
+            for entry in walkdir::WalkDir::new(&src)
+                .min_depth(1)
+                .into_iter()
+                .filter_entry(|e| exclude.contains(e.file_name()))
+            {
+                // XXX: some stat calls could be saved here on Windows (metadata is already
+                //   populated but the append* functions call stat again)
+                let entry = entry?;
+                let file_type = entry.file_type();
+                let path = entry.path();
+                let rel_path = path.strip_prefix(&src).unwrap();
+                if file_type.is_file() {
+                    a.append_file(rel_path, &mut fs::File::open(path)?)?;
+                } else if file_type.is_dir() {
+                    a.append_dir(rel_path, path)?;
+                } else if file_type.is_symlink() {
+                    let mut header = tar::Header::new_gnu();
+                    header.set_metadata(&entry.metadata()?);
+                    a.append_link(&mut header, rel_path, fs::read_link(&path)?)?;
+                } else {
+                    a.append_path_with_name(path, rel_path)?;
+                }
+            }
+            a.into_inner()?.finish()?;
+            Ok(())
+        })
     }
 }
